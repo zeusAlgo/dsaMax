97e27eb2da0904997c0a9be3f2b15ef9cf26e316
uz <uz@pop-os.localdomain>
2022-02-06 16:43:35 -0600

    m

--- app/src/main/java/com/alg/dsaMax/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/alg/dsaMax/ui/main/SectionsPagerAdapter.kt
@@ -42,6 +42,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     }
 
     override fun getCount(): Int {
+        // Show 2 total pages.
         return 3
     }
 }
