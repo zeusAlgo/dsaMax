e9cb2154f1b8a693fca6461ad7a7b11bdc4bac94
uz <uz@pop-os.localdomain>
2022-02-06 14:32:43 -0600

    m

--- app/src/main/java/com/alg/dsaMax/MainActivity.kt
+++ app/src/main/java/com/alg/dsaMax/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.alg.dsaMax
-
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
