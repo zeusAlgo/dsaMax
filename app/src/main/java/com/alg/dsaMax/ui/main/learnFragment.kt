7ddb46e64ad1b91e46ff548bddb1812dc9287125
uz <uz@pop-os.localdomain>
2022-02-09 11:56:48 -0600

    m

--- app/src/main/java/com/alg/dsaMax/ui/main/learnFragment.kt
+++ app/src/main/java/com/alg/dsaMax/ui/main/learnFragment.kt
@@ -70,27 +70,27 @@ class learnFragment : Fragment() {
     private fun prevCard(cardTv: TextView, card: View,
                          hashmap: HashMap<Int, Array<Any>>,
                          keyStack: MutableList<Int>) {
-        if (keyStack.size == 1) {
-            toast("Nope")
-        } else {
-            val key = keyStack.last()
-            cardTv.text = hashmap[key]!![0].toString()
-
-            toast(keyStack.toString())
-            card.setOnClickListener { flipCard(cardTv, card, hashmap, key) }
-        }
 //        if (keyStack.size == 1) {
-//            toast("Not Allowed")
-//
-//        } else if (keyStack.size > 1){
-//            val idx = keyStack.removeLast()
-//            cardTv.text = hashmap[idx]!![0].toString()
+//            toast("Nope")
+//        } else {
+//            val key = keyStack.last()
+//            cardTv.text = hashmap[key]!![0].toString()
 //
 //            toast(keyStack.toString())
-//            card.setOnClickListener { flipCard(cardTv, card, hashmap, idx) }
-//        } else {
-//            toast("Not Allowed")
-//        }
+//            card.setOnClickListener { flipCard(cardTv, card, hashmap, key) }
+//
+        if (keyStack.size == 1) {
+            toast("Not Allowed")
+
+        } else if (keyStack.size > 1){
+            val idx = keyStack.removeLast()
+            cardTv.text = hashmap[idx]!![0].toString()
+
+            toast(keyStack.toString())
+            card.setOnClickListener { flipCard(cardTv, card, hashmap, idx) }
+        } else {
+            toast("Not Allowed")
+        }
     }
     private fun nextCard(cardTv: TextView, card: View,
                          hashmap: HashMap<Int, Array<Any>>,
