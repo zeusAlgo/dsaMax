1d39b15d1d8c6844c58aaa2999a67b7174d9c879
uz <uz@pop-os.localdomain>
2022-02-09 10:05:34 -0600

    m

--- app/src/main/java/com/alg/dsaMax/ui/main/learnFragment.kt
+++ app/src/main/java/com/alg/dsaMax/ui/main/learnFragment.kt
@@ -63,7 +63,10 @@ class learnFragment : Fragment() {
     private fun prevCard(cardTv: TextView, card: View,
                          hashmap: HashMap<Int, Array<Any>>,
                          stack: MutableList<Int>) {
-        if (stack.isNotEmpty()) {
+        if (stack.size == 1) {
+            println("Not allowed")
+
+        } else if (stack.size > 1){
             val idx = stack.removeLast()
             cardTv.text = hashmap[idx]!![0].toString()
 //                card.setOnClickListener { flipCard(cardTv, card, hashmap, rand) }
